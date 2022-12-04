efad6bee44951068d6cade84f70485b6518ca17e
Darren Cauthon <darren@cauthon.com>
2022-12-04 08:59:51 -0600

    Get this to run with the first example.

---
+++ lib/advent2022.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative 'advent2022/elf'
+require_relative 'advent2022/version'
+
+module Advent2022
+  class Error < StandardError; end
+end
---
+++ test/test_advent2022.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class TestAdvent2022 < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Advent2022::VERSION
+  end
+end
